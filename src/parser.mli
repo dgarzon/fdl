98594e994280d03612ddeb2934b5fb309835ddd0
Cara Borenstein <cjb2182@columbia.edu>
2013-12-01 12:41:21 -0500

    tests in pranav's framework

---
+++ src/parser.mli
@@ -0,0 +1,58 @@
+type token =
+  | LPAREN
+  | RPAREN
+  | LBRACE
+  | RBRACE
+  | LBRACK
+  | RBRACK
+  | COMMA
+  | TAB
+  | SEMI
+  | PLUS
+  | MINUS
+  | TIMES
+  | DIVIDE
+  | IN
+  | ASSIGN
+  | MOVE
+  | COPY
+  | EQ
+  | NEQ
+  | LT
+  | LEQ
+  | GT
+  | GEQ
+  | NOT
+  | AND
+  | OR
+  | CONTINUE
+  | BREAK
+  | RETURN
+  | IF
+  | THEN
+  | ELSE
+  | FOR
+  | WHILE
+  | DO
+  | DEF
+  | VOID
+  | INT
+  | STR
+  | DICT
+  | LIST
+  | PATH
+  | BOOL
+  | TRASH
+  | TRUE
+  | FALSE
+  | PRINT
+  | PATHNAME
+  | PATHCREATED
+  | PATHKIND
+  | LIT_INT of (int)
+  | LIT_STR of (string)
+  | ID of (string)
+  | EOF
+
+val program :
+  (Lexing.lexbuf  -> token) -> Lexing.lexbuf -> Ast.program
