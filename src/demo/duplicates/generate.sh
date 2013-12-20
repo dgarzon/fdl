aebf634cdd47649f5b4b626f713cb91e36d1caf6
Daniel Garzon <dg2796@columbia.edu>
2013-12-20 12:48:17 -0500

    Before pulling

--- src/demo/duplicates/generate.sh
+++ src/demo/duplicates/generate.sh
@@ -2,7 +2,9 @@
 
 for i in {1..5}
 do
-   type NUL > 'documentation'.pdf
-   type NUL > 'documentation'.tex
-   type NUL > 'presentation'.ppt
+   type NUL > 'documentation-plt'.pdf
+   type NUL > 'documentation-plt'.tex
+   type NUL > 'presentation-plt'.ppt
+   type NUL > 'presentation-plt'.doc
+   type NUL > 'presentation-plt'.docx
 done
